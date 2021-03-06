c3cde3fb5f30c2caec7847e9aabc6de0e3cd728d
Will <72624648+wgozzard@users.noreply.github.com>
2021-03-06 01:39:05 -0500

    Create df_parser.py
    Data parser for excel that is built for large spreadsheets with several categories that will seek out a series of categories.  For example, maybe you have a spreadsheet of music albums by years and you want to pull all the albums from 1995, this tool will pull all the albums of 1995 into a data frame so it's easier to sort/locate information.
    
    The second part of the code will loop through and print the categories to help you search. Maybe you forgot what years you have stored, use this tool to print them out without dupes.

---
+++ df_parser.py
@@ -0,0 +1,36 @@
+#STEP ONE: PLEASE MAKE SURE 2.17.21 Currency Matrix_Colab  is loaded
+#PLEASE REMEMBER TO PRESS <------ PLAY BUTTON
+import pandas as pd
+def enter_category(cat_colab):
+  
+  file = 'enter spread sheet file here' 
+  df = pd.read_excel(file, sheet_name='spread sheet to read')
+  df1 = df.dropna() #drop null values
+  
+  if cat_colab == 'Oos' or cat_colab=='oos' or cat_colab =='OOS':
+    cat_colab = cat_colab.upper()
+    df_search = df1[(df1['Colab'] == cat_colab)]
+    return df_search
+  else:
+    x = cat_colab.title()
+    df_search = df1[(df1['Colab'] == x)]
+    return df_search
+try:
+  with open(file, "rb") as f:
+    pk_1 = f.read()
+    print("You are ready to search!")
+except:
+  print("PLEASE LOAD What Ever File that's loaded")
+  
+#This code will list the key search words that you can use above
+file = 'enter spread sheet file here' 
+df = pd.read_excel(file, sheet_name='spread sheet to read')
+df1 = df.dropna()
+num = 0 
+l_nm = []
+for i in df1['Colab']:
+  if i not in l_nm:
+    l_nm.append(i)
+for x in l_nm:
+  num += 1
+  print(num, x)
